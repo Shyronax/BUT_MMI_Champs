8171647be9764de9c41da57697ae47de42d8b620
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2023-01-10 03:11:29 +0100

    MMI c'est quoi intégration

---
+++ presentation/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../bootsrapfiles.php'; ?>
+    <title>BUT MMI</title>
+</head>
+
+<body>
+    <?php include '../menu.php'; ?>
+    <div class="container">
+        <main class='d-flex flex-column justify-conte,t-center'>
+            <header class='mb-5 d-flex flex-column align-items-center container'>
+                <h1 class='display-3 text-center '>MMI, c'est quoi?</h1>
+                <div class="row ">
+                    <div class="col-12 col-lg-7">
+                        <p class='align-left'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolorem nihil dolorum facere quibusdam? Impedit cupiditate qui, voluptate est saepe vitae, eveniet blanditiis magni quo velit, non fugit sunt ratione?
+                        </p>
+                    </div>
+                </div>
+            </header>
+            <div class="container mb-5">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-12 col-lg-7 ">
+                        <h2 class='display-6 '>MMI c'est <b> une formation</b></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolorem voluptates, quia commodi repellat omnis? Ad fugit, deserunt quod tempore consectetur unde modi quis, quisquam officia sint magni repudiandae non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ipsum adipisci praesentium. Non perspiciatis quos enim hic repellendus maiores, blanditiis quod temporibus distinctio aperiam quisquam veniam eius vel quia incidunt.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolorem voluptates, quia commodi repellat omnis? Ad fugit, deserunt quod tempore consectetur unde modi quis, quisquam officia sint magni repudiandae non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ipsum adipisci praesentium. Non perspiciatis quos enim hic repellendus maiores, blanditiis quod temporibus distinctio aperiam quisquam veniam eius vel quia incidunt.</p>
+
+                    </div>
+                    <div class="col-12 col-lg-5">
+                        <img class="img-fluid" src="https://www.booska-p.com/wp-content/uploads/2022/07/The-Rock-Visu-News-1024x750.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="container mb-5">
+                <div class="row ">
+                    <div class="col-12 col-lg-5 d-flex flex-column justify-content-center">
+                        <img class="img-fluid" src="https://www.booska-p.com/wp-content/uploads/2022/07/The-Rock-Visu-News-1024x750.jpg" alt="">
+                    </div>
+                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center">
+                        <h2 class='display-6 '>MMI c'est <b>un etat d'esprit</b> </h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolorem voluptates, quia commodi repellat omnis? Ad fugit, deserunt quod tempore consectetur unde modi quis, quisquam officia sint magni repudiandae non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ipsum adipisci praesentium. Non perspiciatis quos enim hic repellendus maiores, blanditiis quod temporibus distinctio aperiam quisquam veniam eius vel quia incidunt.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolorem voluptates, quia commodi repellat omnis? Ad fugit, deserunt quod tempore consectetur unde modi quis, quisquam officia sint magni repudiandae non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ipsum adipisci praesentium. Non perspiciatis quos enim hic repellendus maiores, blanditiis quod temporibus distinctio aperiam quisquam veniam eius vel quia incidunt.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container mb-5">
+                <img class='img-fluid' src="https://fr.web.img6.acsta.net/r_1280_720/newsv7/21/09/06/11/58/3970126.jpg" alt="">
+            </div>
+            <div class="container mb-5">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-12 col-lg-7 ">
+                        <h2 class='display-6 '>MMI c'est <b> une formation</b></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolorem voluptates, quia commodi repellat omnis? Ad fugit, deserunt quod tempore consectetur unde modi quis, quisquam officia sint magni repudiandae non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ipsum adipisci praesentium. Non perspiciatis quos enim hic repellendus maiores, blanditiis quod temporibus distinctio aperiam quisquam veniam eius vel quia incidunt.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolorem voluptates, quia commodi repellat omnis? Ad fugit, deserunt quod tempore consectetur unde modi quis, quisquam officia sint magni repudiandae non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ipsum adipisci praesentium. Non perspiciatis quos enim hic repellendus maiores, blanditiis quod temporibus distinctio aperiam quisquam veniam eius vel quia incidunt.</p>
+
+                    </div>
+                    <div class="col-12 col-lg-5">
+                        <img class="img-fluid" src="https://www.booska-p.com/wp-content/uploads/2022/07/The-Rock-Visu-News-1024x750.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    </div>
+    <?php include '../footer.php' ?>
+</body>
+
+</html>
