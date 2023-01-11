577eeb1d0330871081f09da51d4dbcff1f7b363b
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2023-01-11 01:55:28 +0100

    Added flash info

---
+++ flash-info/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../bootsrapfiles.php'; ?>
+    <title>Flash' info</title>
+</head>
+
+<body>
+    <?php include '../menu.php'; ?>
+    <div class="container mb-5 mt-2">
+        <main class='d-flex flex-column justify-conte,t-center'>
+            <header class='mb-5 d-flex flex-column align-items-center container'>
+                <h1 class='display-3 text-center '>Flash' info!</h1>
+                <div class="row d-flex justify-content-center ">
+                    <div class="col-12 col-lg-7 ">
+                        <p class='align-left'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolorem nihil dolorum facere quibusdam? Impedit cupiditate qui, voluptate est saepe vitae, eveniet blanditiis magni quo velit, non fugit sunt ratione?
+                        </p>
+                    </div>
+                </div>
+            </header>
+            <div class="container mb-5">
+                <div class="card mb-2">
+                    <div class="card-header">
+                        Title
+                    </div>
+
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>15/15/15</p>
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="card mb-2">
+                    <div class="card-header">
+                        Title
+                    </div>
+
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>15/15/15</p>
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div class="card mb-2">
+                    <div class="card-header">
+                        Title
+                    </div>
+
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>15/15/15</p>
+                            <p>A well-known quote, contained in a blockquote element.</p>
+                            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+    </div>
+    </main>
+    <?php include '../footer.php' ?>
+</body>
+
+</html>
