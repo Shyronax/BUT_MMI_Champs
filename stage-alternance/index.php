475f707c17250e06cadb376f7a8737e4a215e327
xshuky <itsjuneforwork@gmail.com>
2023-01-13 17:17:18 +0100

    correction position cercle

--- stage-alternance/index.php
+++ stage-alternance/index.php
@@ -30,10 +30,10 @@
                 <input class='radio' id='work5' name='works' type='radio' checked>
                 <div class="relative">
                 <label for='work5'>MMI1 : Stage de 2 semaines minimum</label>
-                <span class='date'>À partir de juin-juillet</span>
                 <span class='circle'></span>
                 </div>
                 <div class='content'>
+                <p class='date'>À partir de juin-juillet</p>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ea necessitatibus quo velit natus cupiditate qui alias possimus ab praesentium nostrum quidem obcaecati nesciunt! Molestiae officiis voluptate excepturi rem veritatis eum aliquam qui laborum non ipsam ullam tempore reprehenderit illum eligendi cumque mollitia temporibus! Natus dicta qui est optio rerum.
                 </p>
@@ -42,11 +42,12 @@
             <li class='work'>
                 <input class='radio' id='work3' name='works' type='radio'>
                 <div class="relative">
-                <label for='work3'>Lorem ipsum dolor sit amet</label>
-                <span class='date'>10 May 2013</span>
+                <label for='work3'>MMI2 : Stage de 10 semaines minimum</label>
+                
                 <span class='circle'></span>
                 </div>
                 <div class='content'>
+                <p class='date'>Entre avril et juin</p>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio ea necessitatibus quo velit natus cupiditate qui alias possimus ab praesentium nostrum quidem obcaecati nesciunt! Molestiae officiis voluptate excepturi rem veritatis eum aliquam qui laborum non ipsam ullam tempore reprehenderit illum eligendi cumque mollitia temporibus! Natus dicta qui est optio rerum.
                 </p>
