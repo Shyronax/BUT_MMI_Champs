4678f47dd9b77fdbe0c3fbea9a06b19d14625179
xshuky <itsjuneforwork@gmail.com>
2023-01-15 22:19:06 +0100

    new bdd

--- mmi-bdd.sql
+++ mmi-bdd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 12, 2022 at 04:46 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Hôte : 127.0.0.1:3306
+-- Généré le : dim. 15 jan. 2023 à 21:07
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,17 +18,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mmichamps`
+-- Base de données : `mmi`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `article`
+-- Structure de la table `article`
 --
 
-CREATE TABLE `article` (
-  `id_article` int(4) NOT NULL,
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE IF NOT EXISTS `article` (
+  `id_article` int(4) NOT NULL AUTO_INCREMENT,
   `nom_article_fr` varchar(100) NOT NULL,
   `contenu_article_fr` varchar(1000) NOT NULL,
   `nom_article_en` varchar(100) NOT NULL,
@@ -37,48 +38,71 @@ CREATE TABLE `article` (
   `date_article` date NOT NULL,
   `synopsis_fr` varchar(200) NOT NULL,
   `miniature_article` varchar(50) NOT NULL,
-  `ext_auteur` int(11) NOT NULL
+  `ext_auteur` int(11) NOT NULL,
+  PRIMARY KEY (`id_article`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `projet`
+-- Structure de la table `matiere`
 --
 
-CREATE TABLE `projet` (
-  `id_projet` int(2) NOT NULL,
+DROP TABLE IF EXISTS `matiere`;
+CREATE TABLE IF NOT EXISTS `matiere` (
+  `id_matiere` int(2) NOT NULL,
+  `nom_matiere` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `ext_projet` int(11) NOT NULL,
+  `ext_utilisateur` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projet`
+--
+
+DROP TABLE IF EXISTS `projet`;
+CREATE TABLE IF NOT EXISTS `projet` (
+  `id_projet` int(2) NOT NULL AUTO_INCREMENT,
   `nom_projet` varchar(50) NOT NULL,
   `etudiants` varchar(100) NOT NULL,
   `date_projet` date NOT NULL,
   `niveau` varchar(10) NOT NULL,
   `description_fr` varchar(500) NOT NULL,
-  `description_en` varchar(500) NOT NULL
+  `description_en` varchar(500) NOT NULL,
+  `img_projet` varchar(50) NOT NULL,
+  `lien` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_projet`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `temoignage`
+-- Structure de la table `temoignage`
 --
 
-CREATE TABLE `temoignage` (
-  `id_temoignage` int(2) NOT NULL,
+DROP TABLE IF EXISTS `temoignage`;
+CREATE TABLE IF NOT EXISTS `temoignage` (
+  `id_temoignage` int(2) NOT NULL AUTO_INCREMENT,
   `etudiant` varchar(100) NOT NULL,
   `promo` varchar(15) NOT NULL,
   `nom_temoignage` varchar(50) NOT NULL,
   `contenu_temoignage_fr` varchar(1000) NOT NULL,
-  `contenu_temoignage_en` varchar(1000) NOT NULL
+  `contenu_temoignage_en` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id_temoignage`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateur`
+-- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
-  `id_prof` int(3) NOT NULL,
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_prof` int(3) NOT NULL AUTO_INCREMENT,
   `nom_prof` varchar(50) NOT NULL,
   `login` varchar(20) NOT NULL,
   `mdp` varchar(20) NOT NULL,
@@ -91,64 +115,9 @@ CREATE TABLE `utilisateur` (
   `p_articles` tinyint(4) NOT NULL,
   `p_projets` tinyint(4) NOT NULL,
   `p_temoignages` tinyint(4) NOT NULL,
-  `p_admin` tinyint(4) NOT NULL
+  `p_admin` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id_prof`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `article`
---
-ALTER TABLE `article`
-  ADD PRIMARY KEY (`id_article`);
-
---
--- Indexes for table `projet`
---
-ALTER TABLE `projet`
-  ADD PRIMARY KEY (`id_projet`);
-
---
--- Indexes for table `temoignage`
---
-ALTER TABLE `temoignage`
-  ADD PRIMARY KEY (`id_temoignage`);
-
---
--- Indexes for table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`id_prof`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `article`
---
-ALTER TABLE `article`
-  MODIFY `id_article` int(4) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `projet`
---
-ALTER TABLE `projet`
-  MODIFY `id_projet` int(2) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `temoignage`
---
-ALTER TABLE `temoignage`
-  MODIFY `id_temoignage` int(2) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `id_prof` int(3) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
