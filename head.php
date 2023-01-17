4579187562e2a8f571a89f06f5804d6dad8c3274
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2023-01-18 00:36:25 +0100

    favicon +eiffel

--- head.php
+++ head.php
@@ -7,4 +7,5 @@
 <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<link rel="shortcut icon" href="https://but-mmi.fr/logo/favicon.svg" type="image/x-icon">
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
