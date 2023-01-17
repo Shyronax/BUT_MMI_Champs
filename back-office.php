72576d4ad91bab293c6a2c2786c4f174413c35a7
Shyronax <oscarfpoisson@gmail.com>
2023-01-17 20:40:29 +0100

    test back

--- back-office.php
+++ back-office.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include('bootstrapfiles.php'); include(' model.php');?>
+    <?php include('model.php');?>
     <title>Back-Office MMI Champs</title>
 </head>
 <body>
