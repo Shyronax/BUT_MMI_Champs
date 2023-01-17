e3a2dcbd138e3f5d423b0e1c5cf392d59c56ab01
xshuky <itsjuneforwork@gmail.com>
2023-01-17 23:14:13 +0100

    ajout pp temoignages

--- temoignages.php
+++ temoignages.php
@@ -16,9 +16,43 @@
                     <li class="breadcrumb-item active" aria-current="page">La formation MMI</li>
                 </ol>
             </nav>
-            <header data-aos="fade-down" data-aos-offset="500" class='mb-5 d-flex flex-column align-items-center container d-flex flex-column align-items-center vh100 justify-content-center'>
+            <header data-aos="fade-down" data-aos-offset="500" class='mb-5 d-flex flex-column align-items-center container d-flex flex-column align-items-center vh75 justify-content-center'>
                 <h1 class='display-3 text-center mt-2 mb-5'>Nos étudiants témoignent</h1>
             </header>
+
+            <div data-aos="fade-right" data-aos-offset="200" class="container d-flex flex-column align-items-center  justify-content-center mb-5">
+                <div class="row">
+                    <div class="col-12 col-lg-10 d-flex flex-column justify-content-center bg-body-secondary p-5 rounded-5">
+                        <p>Je suis entrée dans ce DUT afin de devenir Chef de projet e-commerce.
+                            Pour des raisons personnelles j'ai rencontré des difficultés, mais l'équipe enseignante m'a aidée à surmonter cette épreuve et j'en suis sortie avec une nouvelle passion l'UX Design !
+                            MMI m'a permis de me découvrir de nouveaux talents !
+                        </p>
+                        <p><b>LÉA, PROMO 2019</b></p>
+                    </div>
+                    <div class="col-12 col-lg-2">
+                        <img class="img-fluid" src="src/img/illustration/mmi-formation.webp" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div data-aos="fade-left" data-aos-offset="200" class="container d-flex flex-column align-items-center  justify-content-center mb-5">
+                <div class="row ">
+                    <div class="col-12 col-lg-5 d-flex flex-column justify-content-center">
+                        <img class="img-fluid" src="src/img/illustration/mmi-ce-qu-on-apprend.webp" alt="">
+                    </div>
+                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center">
+                        <h2 class='display-6 '>Ce qu'on apprend</h2>
+                        <p>Les compétences acquises lors de ces trois années de formation tournent autour de <b>trois parcours</b> :</p>
+                        <ul>
+                            <li><b>Développement web et dispositifs interactifs</b> : développement Web, base de données, services sur réseau et gestion de contenu.</li>
+                            <li><b>Création numérique</b> : savoir générer du contenu multimédia (son, vidéo, photo, montage et animation 2D/3D) et utilisation des logiciels de la suite adobe ( illustrator, premiere pro, indesign…).</li>
+                            <li><b>Stratégie de communication numérique et design d'expérience</b> : expression et communication, théorie de la communication</li>
+                        </ul>
+                        <br>
+                        <a href="matieres.php" class="btn btn-primary w-25">En savoir plus</a>
+                    </div>
+                </div>
+            </div>
 
             <div data-aos="fade-right" data-aos-offset="300" class="container">
                 <div class="row d-flex flex-column justify-content-center align-items-center">
