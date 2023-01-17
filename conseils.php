91b0998f84ac62d4f92f3c660153b828c93585db
xshuky <itsjuneforwork@gmail.com>
2023-01-18 00:14:32 +0100

    finiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- conseils.php
+++ conseils.php
@@ -8,13 +8,13 @@
 
 <body>
     <?php include 'menu.php'; ?>
-    <div class="container w-75">
+    <div class="container">
         <main class='d-flex flex-column justify-content-center'>
-            <header class='mb-5 mt-3 d-flex flex-column align-items-center container'>
+            <header data-aos="fade-down" data-aos-offset="500" class='mb-5 d-flex flex-column align-items-center  justify-content-center container vh50'>
                 <h1 class='display-3 text-center '>Nos p'tits conseils</h1>
                 <div class="row d-flex justify-content-center ">
                     <div class="col-12 col-lg-7 ">
-                        <p class='align-left'>
+                        <p class='text-center'>
                             Ici, on vous donne nos conseils en tant qu'étudiant pour réussir en MMI ! 
                         </p>
                     </div>
@@ -22,56 +22,30 @@
             </header>
             <div data-aos="fade-right" data-aos-offset="200" class="container d-flex flex-column align-items-center  justify-content-center mb-5">
                 <div class="row">
-                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center">
-                        <h2 class='display-6 '>Préparez-vous</h2>
+                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center my-3">
+                        <h2>Préparez-vous !</h2>
                         <p>La formation MMI est pluridisciplinaire, beaucoup de sujets sont traités . Il faut donc savoir être autonome et savoir gérer son temps. En vue des nombreux travaux à faire,il ne faut pas oublier d'avoir un disque dur pour sauvegarder ses données!</p>
                     </div>
-                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center">
-                        <h2 class='display-6 '>La charge de travail</h2>
+                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center my-3">
+                        <h2>La charge de travail</h2>
                         <p>Parfois,la charge de travail demandée parait insurmontable et pourtant ce n'est pas le cas ! Il suffit de bien savoir s'organiser et gérer son temps !
 </p>
                     </div>
-                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center">
-                        <h2 class='display-6 '>Travaillez chez soi</h2>
+                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center my-3">
+                        <h2>Travaillez chez soi</h2>
                         <p>Bien que la majorité du travail se fait en cours, il faut toujours prévoir du temps pour travailler chez soi afin de rendre un travail parfait .
 Avant de candidater : pensez à bien vous renseigner en amont sur le contenu du programme du BUT MMI pour savoir si la formation vous convient.
 </p>
                     </div>
-                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center">
-                        <h2 class='display-6 '>Travaillez chez soi</h2>
-                        <p>Bien que la majorité du travail se fait en cours, il faut toujours prévoir du temps pour travailler chez soi afin de rendre un travail parfait .
-Avant de candidater : pensez à bien vous renseigner en amont sur le contenu du programme du BUT MMI pour savoir si la formation vous convient.
+                    <div class="col-12 col-lg-7 d-flex flex-column justify-content-center my-3">
+                        <h2>Organisation</h2>
+                        <p>En MMI c'est crucial ! Cela vous permettra de travailler plus sereinement et surtout plus efficacement. Vos journées seront alors optimisées !
+
 </p>
                     </div>
-
-
-                        <p>
-                            Alliant la théorie à la pratique, la formation propose de développer des compétences en programmation, graphisme ou encore en communication. Cela permet à l'étudiant de devenir polyvalent dans la conception, la réalisation et la commercialisation de produits et de services web et multimédia.
-
-                        </p>
-                        <p>
-                            La formation dure 3 ans et comprend 2600 heures d'enseignement. Lors de la deuxième année, les étudiants pourront s'orienter vers <b>la création numérique </b> ou <b>le développement web </b>
-
-                        </p>
-                        <p>
-                            Le BUT MMI est professionnalisant, il permet de déployer des compétences d'autonomie avec la conduite de nombreux projets individuels ou de groupe.
-                        </p>
-                    </div>
-                    <div class="col-12 col-lg-5">
-                        <img class="img-fluid" src="src/img/illustration/mmi-formation.webp" alt="">
-                    </div>
-                </div>
-            </div>
-        </main>
-        <div class="container mb-2">
-            <h2>Préparez-vous</h2>
-            <p>La formation MMI est pluridisciplinaire, beaucoup de sujets sont traités . Il faut donc savoir être autonome et savoir gérer son temps. En vue des nombreux travaux à faire,il ne faut pas oublier d'avoir un disque dur pour sauvgarder ses données!</p>
-        </div>
-        <div class="container mb-2">
-            <h2>C'est chaud un peu</h2>
-            <p>Parfois,la charge de travail demandé parait insurmontable et pourtant ce n'est pas le cas! il suffit de bien savoir s'organiser et gérer son temps !</p>
-        </div>
     </div>
+</main>
+</div>
     <?php include 'footer.php' ?>
 </body>
 
