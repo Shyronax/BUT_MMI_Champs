4050cd328acc3cd4430d3ba1302a86f8113805a8
xshuky <itsjuneforwork@gmail.com>
2023-01-17 22:08:56 +0100

    regardez c'est nous

--- a-propos.php
+++ a-propos.php
@@ -13,6 +13,30 @@
             <header data-aos="fade-down" data-aos-offset="500" class='mb-5 d-flex flex-column align-items-center justify-content-center container vh50'>
                 <h1 class='display-3 text-center '>À propos</h1>
             </header>
+            <div data-aos="fade-right" data-aos-offset="200" class="container d-flex flex-column align-items-center justify-content-center mb-5">
+                <div class="row d-flex align-items-center  justify-content-center">
+                    <div class="col-12 col-lg-2 d-flex flex-column align-items-center justify-content-center">
+                        <img class="img-fluid" src="src/img/etudiant/clara.png" alt="">
+                        <p>Clara MANY</p>
+                    </div>
+                    <div class="col-12 col-lg-2 d-flex flex-column align-items-center justify-content-center">
+                        <img class="img-fluid" src="src/img/etudiant/corentin.png" alt="">
+                        <p>Corentin MACRET</p>
+                    </div>
+                    <div class="col-12 col-lg-2 d-flex flex-column align-items-center justify-content-center">
+                        <img class="img-fluid" src="src/img/etudiant/elise.png" alt="">
+                        <p>Elise CELADON</p>
+                    </div>
+                    <div class="col-12 col-lg-2 d-flex flex-column align-items-center justify-content-center">
+                        <img class="img-fluid" src="src/img/etudiant/oscar.png" alt="">
+                        <p>Oscar POISSON</p>
+                    </div>
+                    <div class="col-12 col-lg-2 d-flex flex-column align-items-center justify-content-center">
+                        <img class="img-fluid" src="src/img/etudiant/valentin.png" alt="">
+                        <p>Valentin BLEUSE</p>
+                    </div>
+                </div>
+            </div>
             <div class="container mb-2">
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel excepturi itaque facere in, voluptatibus beatae, assumenda quod, error aliquam maxime laudantium accusamus ratione obcaecati ut ad doloremque commodi doloribus? Mollitia?</p>
         </div>
