8844df8626a26c8288cefacde1be4df217b5cff5
xshuky <itsjuneforwork@gmail.com>
2023-01-18 21:52:58 +0100

    enlèvement commentaire

--- head.php
+++ head.php
@@ -3,7 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php include 'bootsrapfiles.php'; ?>
 <link rel="stylesheet" href="style.min.css">
-<!-- <link href="https://api.fontshare.com/v2/css?f[]=clash-display@400,600&display=swap" rel="stylesheet"> -->
 <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
