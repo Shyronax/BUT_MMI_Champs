f42d29a2462745e22d9ce6ecc7900f2da9b62b82
Shyronax <oscarfpoisson@gmail.com>
2023-01-18 18:40:02 +0100

    mini changement css

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php include 'bootsrapfiles.php'; ?>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.min.css">
 <!-- <link href="https://api.fontshare.com/v2/css?f[]=clash-display@400,600&display=swap" rel="stylesheet"> -->
 <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
