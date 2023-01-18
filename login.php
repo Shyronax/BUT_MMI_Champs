783e12f637587301e8d5a9dc2825b08faf8ba198
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2023-01-18 19:26:13 +0100

    test login style

--- login.php
+++ login.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include('head.php'); include('model.php'); ?>
+    <?php include('head.php');
+    include('model.php'); ?>
     <title>Connexion</title>
 </head>
+
 <body>
-<form action="traite_login.php" method="post">
-<div class="mb-3 container d-flex flex-column justify-content-center align-items-center">
-    <h1>Connexion</h1>
-    <?php if(isset($_GET['err'])){
-        if ($_GET['err'] == 'login'){
-            echo '<p class="text-danger">Vos identifiants sont incorrects, veuillez réessayer.</p>';
-        }
-    } ?>
-    <label class='form-label' for="username">Nom d'utilisateur</label>
-    <input class='form-control' type="text" name="username" id="">
-    <label class='form-label' for="mdp">Mot de passe</label>
-    <input class='form-control' type="password" name="mdp" id="">
-    <input class='btn btn-primary my-4' type="submit" value="Se connecter">
-</div>
-</form>
+    <form action="traite_login.php" method="post">
+        <div class="mb-3 container h-100 d-flex flex-column justify-content-center align-items-center w-25">
+            <h1>Connexion</h1>
+            <?php if (isset($_GET['err'])) {
+                if ($_GET['err'] == 'login') {
+                    echo '<p class="text-danger">Vos identifiants sont incorrects, veuillez réessayer.</p>';
+                }
+            } ?>
+            <label class='form-label' for="username">Nom d'utilisateur</label>
+            <input class='form-control' type="text" name="username" id="">
+            <label class='form-label' for="mdp">Mot de passe</label>
+            <input class='form-control' type="password" name="mdp" id="">
+            <input class='btn btn-primary my-4' type="submit" value="Se connecter">
+        </div>
+    </form>
 </body>
+
 </html>
