1c5531b4e3dfdcada6106a727cdf2d7889d968ce
Shyronax <oscarfpoisson@gmail.com>
2023-01-19 00:53:49 +0100

    change type number

--- edit_projet.php
+++ edit_projet.php
@@ -26,7 +26,7 @@ $projet = getProjet($_GET['id']);
     <textarea name="description" id="" cols="30" rows="10"><?php echo(htmlspecialchars($projet['description'])); ?></textarea>
     <label class="form-label" for="lien">Lien</label>
     <input class="form-control" type="url" name="lien" id="" value="<?php echo($projet['lien']); ?>">
-    <input type="checkbox" name="id" id="" value="<?php echo($_GET['id']) ?>" style="display: none;">
+    <input type="number" name="id" id="" value=<?php echo($_GET['id']) ?> style="display: none;">
     <input class="btn btn-primary my-4" type="submit" value="Envoyer">
     </div>
 </form>
--- script_edit_matiere.php
+++ script_edit_matiere.php
@@ -1,6 +1,5 @@
 <?php
 include('model.php');
 editMatiere($_POST['id'], $_POST['nom'], $_POST['description']);
-var_dump($_POST['description'])
-// header('Location: back-office.php');
+header('Location: back-office.php');
 ?>
