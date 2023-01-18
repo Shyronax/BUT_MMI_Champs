50c8c769a071005ba2684c3662c24610162ff88b
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2023-01-18 19:32:32 +0100

    added register header

--- register_projet.php
+++ register_projet.php
@@ -1,4 +1,4 @@
 <?php
 include('model.php');
 addProjet($_POST['nom'], $_POST['etudiants'], $_POST['annee'], $_POST['niveau'], $_POST['iframe'], $_POST['lien'], $_FILES['image']['tmp_name'], $_POST['description'],);
-?>
+header('Location: back-office.php');
