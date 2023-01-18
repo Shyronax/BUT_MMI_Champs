7e3a26c985bcda301dc32d4691d2db649eb1f9eb
Shyronax <oscarfpoisson@gmail.com>
2023-01-18 16:11:26 +0100

    typo

--- traite_login.php
+++ traite_login.php
@@ -1,7 +1,7 @@
 <?php include('model.php');
 if(login($_POST['username'], $_POST['mdp'])){
     header('Location: back-office');
-// } else {
+}// else {
 //     header('Location: login.php?err=login');
 // };
 ?>
