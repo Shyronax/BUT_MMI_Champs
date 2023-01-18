fd2f60c5cb2604e94ce3faa7e519ffeff03c9885
Shyronax <oscarfpoisson@gmail.com>
2023-01-19 00:48:31 +0100

    test type number

--- script_edit_matiere.php
+++ script_edit_matiere.php
@@ -1,7 +1,5 @@
 <?php
 include('model.php');
 editMatiere($_POST['id'], $_POST['nom'], $_POST['description']);
-var_dump($_POST['nom']);
-var_dump($_POST['id']);
-// header('Location: back-office.php');
+header('Location: back-office.php');
 ?>
