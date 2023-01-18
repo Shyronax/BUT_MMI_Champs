144ce2735adee421889854347430e5defdd1106d
Shyronax <oscarfpoisson@gmail.com>
2023-01-19 00:33:59 +0100

    test get

--- script_edit_article.php
+++ script_edit_article.php
@@ -1,8 +1,8 @@
 <?php
 include('model.php');
 var_dump($_POST['titre']);
-editArticle($_POST['id'], $_POST['titre'], $_POST['contenu']);
+editArticle($_GET['id'], $_POST['titre'], $_POST['contenu']);
 var_dump($_POST['titre']);
-var_dump($_POST['id']);
+var_dump($_GET['id']);
 // header('Location: back-office.php');
 ?>
