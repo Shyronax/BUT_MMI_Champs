67fb1f697ed6a8fe05c078075355742536df0fcc
Shyronax <oscarfpoisson@gmail.com>
2023-01-18 16:10:29 +0100

    marche toujours pas

--- traite_login.php
+++ traite_login.php
@@ -1,7 +1,7 @@
 <?php include('model.php');
 if(login($_POST['username'], $_POST['mdp'])){
     header('Location: back-office');
-} else {
-    header('Location: login.php?err=login');
-};
+// } else {
+//     header('Location: login.php?err=login');
+// };
 ?>
