f210db01dbc6803a6980d926bf83baee9dd7e52e
xshuky <itsjuneforwork@gmail.com>
2023-01-18 16:18:03 +0100

    faq fini

--- faq.php
+++ faq.php
@@ -9,26 +9,57 @@
 <body>
     <?php include 'menu.php'; ?>
     <div class="container w-75">
-        <main class='d-flex flex-column justify-content-center'>
-            <header class='mb-5 mt-3 d-flex flex-column align-items-center container'>
+    <main class='d-flex flex-column justify-content-center'>
+            <header data-aos="fade-down" data-aos-offset="500" class='mb-5 d-flex flex-column align-items-center  justify-content-center container vh50'>
                 <h1 class='display-3 text-center '>FAQ</h1>
                 <div class="row d-flex justify-content-center ">
                     <div class="col-12 col-lg-7 ">
-                        <p class='align-left'>
+                        <p class='text-center'>
                             Une page spécialement dédiée à toutes les questions que vous vous posez !
                         </p>
                     </div>
                 </div>
             </header>
-        </main>
-        <div class="container mb-2">
-            <h2>C'est quoi une journée type en MMI ?</h2>
-            <p>Généralement, vous avez cours de 8h15 à 12h30 pour la matinée et de 13h30 à 17h45 l'après midi. Chaque cours dure environ 2h ensuite vous avez une pause de 15 minutes.  </p>
-        </div>
-        <div class="container mb-2">
-            <h2>Est-ce qu'on peut arreter la formation au bout de 2 ans ?</h2>
-            <p>Oui, vous pouvez mais vous obtiendrez seulement le niveau de diplome " DUT ".</p>
-        </div>
+            <div data-aos="fade-right" data-aos-offset="200" class="container d-flex flex-column align-items-center  justify-content-center mb-5">
+                <div class="row">
+                    <div class="col-12 col-lg-10 col-md-8 col-sm-12 d-flex flex-column justify-content-center my-3">
+                        <h2>C'est quoi une journée type en MMI ?</h2>
+                        <p>Il n'y a pas de journée type en MMI, chaque semaine est différente avec des cours divers. Les journées peuvent aller de 8h15 à 17h45, avec des cours d'une 1h30 à 2h avec des pauses de 15 minutes.</p>
+                    </div>
+                    <div class="col-12 col-lg-10 col-md-8 col-sm-12 d-flex flex-column justify-content-center my-3">
+                        <h2>Est-ce qu'on peut arrêter la formation au bout de 2 ans ?</h2>
+                        <p>Oui, vous pouvez mais vous obtiendrez seulement le niveau de diplome "DUT".
+</p>
+                    </div>
+                    <div class="col-12 col-lg-10 col-md-8 col-sm-12 d-flex flex-column justify-content-center my-3">
+                        <h2>Comment accéder à l'IUT en transport en commun ?</h2>
+                        <p>Situé à 15 minutes à pied du RER A, le BUT MMI est facilement accessible en transports en prenant : </p>
+                        <ul>
+                            <li>RER A : station Noisy-Champs</li>
+                            <li>Bus ligne 212, 213, 312 : arrêt Nobel</li>
+                        </ul>
+                        <div class="col-12 col-lg-10 col-sm-6 overflow-hidden"><iframe class="rounded" src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d955.6743561309602!2d2.5839759004617586!3d48.837146438660476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7e5ced48ab7fc8df!2sIUT%20de%20Marne-la-Vall%C3%A9e%20-%20Universit%C3%A9%20Gustave%20Eiffel!5e0!3m2!1sfr!2sca!4v1673112834622!5m2!1sfr!2sca" width="700" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
+                        
+                    </div>
+                    <div class="col-12 col-lg-10 col-md-8 col-sm-12 d-flex flex-column justify-content-center my-3">
+                        <h2>Est-ce qu'on peut arreter la formation au bout de 2 ans ?</h2>
+                        <p>Oui, vous pouvez mais vous obtiendrez seulement le niveau de diplome "DUT".
+</p>
+                    </div>
+                    
+                    <div class="col-12 col-lg-10 col-md-8 col-sm-12 d-flex flex-column justify-content-center my-3">
+                        <h2>Que se passe-t-il en cas d'absence ?</h2>
+                        <p>Toute absence doit être justifiée par écrit auprès des enseignants et des secrétariats concernés. Les motifs d'absence suivants peuvent être considérés comme justifiés : certificat médical daté et tamponné, acte officiel , convocation nominative , tout cas de force majeure laissée à l'appréciation du chef de département (ou son représentant).
+</p>
+                    </div>
+                    <div class="col-12 col-lg-10 col-md-8 col-sm-12 d-flex flex-column justify-content-center my-3">
+                        <h2>Quelles sont les outils qui sont mis à disposition ?</h2>
+                        <p>À l'IUT de Champs-sur-Marne, des ordinateurs (dont une salle avec des ordinateurs Mac) incluant les logiciels Adobe, permettant ainsi aux étudiants d'avoir accès aux outils nécessaires pour les travaux.</p>
+                        <p>Du matériel vidéo, ainsi qu'une salle "fond vert" et une cabine son offrent la possibilité aux élèves de réaliser leur travaux audiovisuels.</p>
+                        <p>Une bibliothèque physique et numérique est également disponible pour les étudiants pour se documenter.</p>
+                    </div>
+    </div>
+</main>
     </div>
     <?php include 'footer.php' ?>
 </body>
