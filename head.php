69dfd957bc1d29ecbc3d7d374c4055a5b057a64f
Valentin-Bleuse <98829094+Valentin-Bleuse@users.noreply.github.com>
2023-01-18 17:04:39 +0100

    favicon

--- head.php
+++ head.php
@@ -7,5 +7,5 @@
 <link href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,400&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-<link rel="shortcut icon" href="https://but-mmi-champs.fr/logo/favicon.svg" type="image/x-icon">
+<link rel="shortcut icon" href="https://but-mmi-champs.fr/logo/favicon1.svg" type="image/x-icon">
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
