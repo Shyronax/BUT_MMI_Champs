fa58602937561f14fd20bb97167a219b42d82317
Shyronax <oscarfpoisson@gmail.com>
2023-01-19 01:47:34 +0100

    delee var_dump

--- script_edit_article.php
+++ script_edit_article.php
@@ -1,8 +1,5 @@
 <?php
 include('model.php');
-var_dump($_POST['titre']);
 editArticle($_POST['id'], $_POST['titre'], $_POST['contenu']);
-var_dump($_POST['titre']);
-var_dump($_POST['id']);
-// header('Location: back-office.php');
+header('Location: back-office.php');
 ?>
